4c96ae1ee84a6c566f1fbb66d031d958154927fe
Omaro Taylor <smart.omarodev@gmail.com>
2018-10-19 11:15:18 -0500

    Add Layout folder with SEO feature

--- motiion/src/pages/index.js
+++ motiion/src/pages/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import { Layout } from '../layouts'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <React.Fragment>
+    <Layout
+      pageTitle='Home'
+      meta={[
+        { name: 'description', content: 'Mottion PWA' },
+        { name: 'keywords', content: 'Mottion, Gatsby, PWA' },
+      ]}
+    >
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </React.Fragment>
 )
 
 export default IndexPage
