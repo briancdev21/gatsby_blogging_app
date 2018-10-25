8efa541fed60b16609ab14e88d7c20612c638027
Omaro Taylor <smart.omarodev@gmail.com>
2018-10-24 20:45:28 -0500

    Update config setting

--- motiion/gatsby-config.js
+++ motiion/gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'Motiion',
+    contentful: {
+      space: 'j364uols58ro',
+      accessToken: '38ae71b3396e5e26dca9fb6b09893ffee94900760d29422658530c84d5fef339',
+    },
   },
   plugins: [
     'gatsby-plugin-react-helmet',
